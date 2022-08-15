b9df30125a5b34c7031078c9debb681217bebb40
Roman Chirkov <r.tchirkov2010@yandex.ru>
2022-08-15 23:14:12 +0300

    Created map for Toy

--- People/Mappings/PeopleProfile.cs
+++ People/Mappings/PeopleProfile.cs
@@ -8,8 +8,8 @@ public class PeopleProfile : Profile
 {
 	public PeopleProfile()
 	{
-		CreateMap<PersonRto, PersonBlo>();
-		CreateMap<PersonBlo, PersonRto>();
-		CreateMap<ChildrenRto, ChildrenBlo>();
+		CreateMap<PersonRto, PersonBlo>().ReverseMap();
+		CreateMap<ChildrenRto, ChildrenBlo>().ReverseMap();
+		CreateMap<ToyRto, ToyBlo>().ReverseMap();
 	}
 }
