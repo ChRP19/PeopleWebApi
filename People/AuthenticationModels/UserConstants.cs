36de20551cb942611c92e454c44690b9c8e53514
Roman Chirkov <r.tchirkov2010@yandex.ru>
2022-08-28 00:14:11 +0300

    Created User constants for emulate database for testing authenticate

---
+++ People/AuthenticationModels/UserConstants.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace People.AuthenticationModels;
+
+public class UserConstants
+{
+	public static List<UserModel> Users = new()
+	{
+		new UserModel { Username = "admin", EmailAddress = "admin@email.com", Password = 
+			"adminPassword", GivenName = "Roman", Surname = "Chirkov", Role = "Administrator" },
+		new UserModel { Username = "user", EmailAddress = "user@email.com", Password = 
+			"userPassword", GivenName = "John", Surname = "Doe", Role = "User" },
+	};
+}
